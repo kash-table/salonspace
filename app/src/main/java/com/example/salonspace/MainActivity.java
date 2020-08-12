06d5a7fd6acae212d7f225dd2b0ab72ce48f5b01
DongSuni <lhsljh123@ajou.ac.kr>
2020-08-12 15:38:09 +0900

    merge

--- app/src/main/java/com/example/salonspace/MainActivity.java
+++ app/src/main/java/com/example/salonspace/MainActivity.java
@@ -10,7 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //testing
+        //testing2
         // Junho Branch Test
+
     }
 }
