d51e155271293d61b1cb703556ef60ccf0b9bcc2
DongSuni <lhsljh123@ajou.ac.kr>
2020-08-12 15:50:36 +0900

    Merge Test

--- app/src/main/java/com/example/salonspace/MainActivity.java
+++ app/src/main/java/com/example/salonspace/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //testing
         //testing2
+
         // Junho Branch Test
 
     }
