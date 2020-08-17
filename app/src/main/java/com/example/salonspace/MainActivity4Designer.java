0bb7d592afbb9ece702df3d411059f0533c2a94d
DongSuni <lhsljh123@ajou.ac.kr>
2020-08-17 23:41:03 +0900

    BottomNavigationView Added

--- app/src/main/java/com/example/salonspace/MainActivity4Designer.java
+++ app/src/main/java/com/example/salonspace/MainActivity4Designer.java
@@ -1,4 +1,46 @@
 package com.example.salonspace;
 
-public class MainActivity4Designer {
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity4Designer extends AppCompatActivity {
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private FragmentDesigner fragmentDesigner = new FragmentDesigner();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_designer);
+
+        // Defalut Main Fragment
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frameLayout, fragmentDesigner).commitAllowingStateLoss();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // Change Fragment
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                switch(item.getItemId()){
+                    case R.id.Item1:
+                        transaction.replace(R.id.frameLayout, fragmentDesigner).commitAllowingStateLoss();
+                        break;
+                    case R.id.Item2:
+                        transaction.replace(R.id.frameLayout, fragmentDesigner).commitAllowingStateLoss();
+                        break;
+                }
+                return false;
+            }
+        });
+
+    }
+
 }
