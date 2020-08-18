8fc11eb51672bc0e89090466e62326101381bd2e
rohmindo <shalseh@naver.com>
2020-08-18 15:22:36 +0900

    Splash

---
+++ app/src/main/java/com/example/salonspace/SplahActivity.java
@@ -0,0 +1,31 @@
+package com.example.salonspace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class SplahActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splah);
+        startLoading();
+    }
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent= new Intent(getApplicationContext(), MainActivity4Designer.class);
+                startActivity(intent);  //juno 메인화면을 띄운다.
+                finish();   //현재 액티비티 종료
+            }
+        }, 3000); // 화면에 Logo 4초간 보이기
+    }
+}
