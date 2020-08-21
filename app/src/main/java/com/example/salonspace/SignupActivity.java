39f02ca5d0534594388ea5a1b128a2ca62776170
woasidh <woasidh@ajou.ac.kr>
2020-08-22 03:49:09 +0900

    connect

--- app/src/main/java/com/example/salonspace/SignupActivity.java
+++ app/src/main/java/com/example/salonspace/SignupActivity.java
@@ -2,15 +2,18 @@ package com.example.salonspace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.RadioButton;
 
 public class SignupActivity extends AppCompatActivity {
     RadioButton rbtn_ctm, rbtn_dsg;
+    Button btn_signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +39,20 @@ public class SignupActivity extends AppCompatActivity {
                 }else{
                     compoundButton.setButtonTintList(ColorStateList.valueOf(Color.parseColor(("#000000"))));
                 }
+            }
+        });
+
+        btn_signup= findViewById(R.id.btn_signup);
+        btn_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                if(rbtn_ctm.isChecked()){
+                    intent.putExtra("usertype", 0);
+                }else{
+                    intent.putExtra("usertype", 1);
+                }
+                startActivity(intent);
             }
         });
     }
