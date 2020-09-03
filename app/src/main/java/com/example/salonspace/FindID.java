69f95e42bcd1e708de7b108a756503daf35fdb64
DongSuni <lhsljh123@ajou.ac.kr>
2020-09-04 03:45:11 +0900

    Popup Added

--- app/src/main/java/com/example/salonspace/FindID.java
+++ app/src/main/java/com/example/salonspace/FindID.java
@@ -2,8 +2,11 @@ package com.example.salonspace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class FindID extends AppCompatActivity {
 
@@ -12,5 +15,13 @@ public class FindID extends AppCompatActivity {
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_i_d);
+
+
+    }
+    public void mOnPopupClick(View v){
+        //데이터 담아서 팝업(액티비티) 호출
+        Intent intent = new Intent(this, PopupActivity.class);
+        intent.putExtra("data", "등록하신 연락처로 아이디를 보내드렸습니다.");
+        startActivityForResult(intent, 1);
     }
 }
