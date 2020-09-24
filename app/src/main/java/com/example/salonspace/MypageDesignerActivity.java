93233b62269183c332b82fb064c6fffd2cd6d555
Rohmindo <shalseh@naver.com>
2020-09-25 04:27:49 +0900

    save image to server

--- app/src/main/java/com/example/salonspace/MypageDesignerActivity.java
+++ app/src/main/java/com/example/salonspace/MypageDesignerActivity.java
@@ -19,18 +19,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
-import org.apache.http.util.EntityUtils;
+import android.widget.Toast;
+
 
 
 
@@ -39,11 +29,18 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.ArrayList;
 
 import static android.app.Activity.RESULT_OK;
@@ -55,6 +52,7 @@ public class MypageDesignerActivity extends Fragment {
     Uri uri;
     String ID="";
     View v1;
+    String filePath;
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v1 = (View) inflater.inflate(R.layout.activity_mypage_designer, container, false);
         Image_profile = v1.findViewById(R.id.profile_image);
@@ -122,7 +120,7 @@ public class MypageDesignerActivity extends Fragment {
                 Log.e("test223", "bb2");
                 final Bundle extras = data.getExtras();
 
-                String filePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + System.currentTimeMillis() + ".jpg";
+                filePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + System.currentTimeMillis() + ".jpg";
 
                 Log.e("test223", "bb");
                 if (extras != null) {
@@ -162,4 +160,97 @@ public class MypageDesignerActivity extends Fragment {
             e.printStackTrace();
         }
     }
+
+
+    protected String doInBackground(String... strings) {
+            /*
+                 strings에서 값 할당 생략
+            */
+        // 전송할 키와 값. 배열보다는 Map을 이용하자.
+        String[] data = {ID};
+        String[] dataName = {"id"};
+
+        // boundary생성, 여기서는 고정값이지만 되도록이면 실행할때마다 다른값을 할당하자.
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "androidupload";
+        String resp="";
+        File targetFile = null;
+        String fileName ="";
+        if(filePath!=null) {
+            targetFile = new File(filePath);
+            fileName=targetFile.getName();
+        }
+
+        byte[] buffer;
+        int maxBufferSize = 5*1024 * 1024;
+        HttpURLConnection conn = null;
+        String urlStr = "http://13.125.176.39/upload_designer_image.php";
+        try {
+            conn = (HttpURLConnection) new URL(urlStr).openConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            conn.setRequestMethod("POST");
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        }
+        conn.setReadTimeout(10000);
+        conn.setConnectTimeout(10000);
+        conn.setDoOutput(true);
+        conn.setDoInput(true);
+        conn.setUseCaches(false);
+        conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+        conn.setRequestProperty("Content-Type","multipart/form-data;boundary="+boundary);
+        String delimiter = twoHyphens + boundary + lineEnd; // --androidupload\r\n
+        StringBuffer postDataBuilder = new StringBuffer();
+        for(int i=0;i<data.length;i++){
+            postDataBuilder.append(delimiter);
+            postDataBuilder.append("Content-Disposition: form-data; name=\"" + dataName[i] +"\""+lineEnd+lineEnd+data[i]+lineEnd);
+        }
+        // 파일이 존재할 때에만 생성
+        if(fileName!=null){
+            postDataBuilder.append(delimiter);
+            postDataBuilder.append("Content-Disposition: form-data; name=\"" + "myfile" + "\";filename=\"" + fileName +"\"" + lineEnd);
+        }
+        try {
+            DataOutputStream ds = new DataOutputStream(conn.getOutputStream());
+            ds.write(postDataBuilder.toString().getBytes());
+
+            if(filePath!=null){
+                ds.writeBytes(lineEnd);
+                FileInputStream fStream = new FileInputStream(targetFile);
+                buffer = new byte[maxBufferSize];
+                int length = -1;
+                while((length=fStream.read(buffer)) != -1){
+                    ds.write(buffer,0,length);
+                }
+                ds.writeBytes(lineEnd);
+                ds.writeBytes(lineEnd);
+                ds.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd); // requestbody end
+                fStream.close();
+            }else{
+                ds.writeBytes(lineEnd);
+                ds.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd); // requestbody end
+            }
+            ds.flush();
+            ds.close();
+            int responseCode  = conn.getResponseCode();
+
+            if(responseCode == HttpURLConnection.HTTP_OK)
+            {
+                String line = null;
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                while((line=br.readLine())!=null) {
+                    resp += line;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return resp;
+    }
+
 }
